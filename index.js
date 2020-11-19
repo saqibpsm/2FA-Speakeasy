8faf1519b93b0fcc7dba10e5c76b3ca2ff9a97b8
Muhammad Saqib <mh.saqibpsm@gmail.com>
2020-11-19 23:43:03 +0500

    Token verified route

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
 
 const app = express()
 
+app.use(express.json())
+
 const db = new JsonDB(new Config('myDatabase', true, false, '/'))
 
 app.get('/api', (req, res) => {
@@ -25,6 +27,31 @@ app.post('/api/register', (req, res) => {
         res.json({ id, secret: temp_secret.base32 })
     } catch (err) {
         res.status(500).json({ message: "Error in generating Secret" })
+    }
+})
+
+app.post('/api/verify', (req, res) => {
+    const {token, userId} = req.body
+
+    try {
+        const path = `/user/${userId}`;
+    const user = db.getData(path);
+    console.log({ user })
+    const { base32: secret } = user.temp_secret;
+    const verified = speakeasy.totp.verify({
+      secret,
+      encoding: 'base32',
+      token
+    })
+
+            if(verified) {
+                db.push(path, { id:userId, secret: user.temp_secret })
+                res.json({ verified: true })
+            } else {
+                res.json({ verified: false })
+            }
+    } catch (err) {
+        res.status(500).json({ message: "Error Finding user" })
     }
 })
 
